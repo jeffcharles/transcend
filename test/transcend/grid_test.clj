05c7372a17c8a6b9f4d5b99d6b49d11b965eff40
Jeffrey Charles <jeffreycharles@gmail.com>
2012-12-28 13:47:50 -0500

    Refactor tests by extracting common get-from-set operations into method

--- test/transcend/grid_test.clj
+++ test/transcend/grid_test.clj
@@ -13,17 +13,13 @@
     (set-grid-value! grid 0 0 :set)
     (is (= [[:set]] @grid))))
 
-(deftest get-from-set
+(defn- get-from-set
+  [x y]
   (let [grid (create-grid)]
-    (set-grid-value! grid 0 0 :set)
-    (is (= :set (get-grid-value grid 0 0)))))
+    (set-grid-value! grid x y :set)
+    (is (= :set (get-grid-value grid x y)))))
 
-(deftest get-from-further-row
-  (let [grid (create-grid)]
-    (set-grid-value! grid 2 1 :set)
-    (is (= :set (get-grid-value grid 2 1)))))
+(deftest get-from-set-00 (get-from-set 0 0))
+(deftest get-from-set-further-row (get-from-set 2 1))
+(deftest get-from-set-further-col (get-from-set 1 2))
 
-(deftest get-from-further-col
-  (let [grid (create-grid)]
-    (set-grid-value! grid 1 2 :set)
-    (is (= :set (get-grid-value grid 1 2)))))
