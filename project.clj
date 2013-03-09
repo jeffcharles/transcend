aaebfecafc533e30827f1bb7a7c473b91a58cac3
Jeffrey Charles <jeffreycharles@gmail.com>
2013-03-09 16:57:45 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject transcend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A very basic spreadsheet that accepts Clojure functions"
+  :url "https://github.com/jeffcharles/transcend"
   :license {:name "The BSD 2-Clause License"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.4.0"]
