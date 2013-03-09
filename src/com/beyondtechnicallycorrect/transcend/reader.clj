5a8bee13205fdf55f6be908bd51cdf961c8a91e3
Jeffrey Charles <jeffreycharles@gmail.com>
2013-03-09 15:17:43 -0500

    Use constant to identify key in reader

--- src/com/beyondtechnicallycorrect/transcend/reader.clj
+++ src/com/beyondtechnicallycorrect/transcend/reader.clj
@@ -1,12 +1,14 @@
 (ns com.beyondtechnicallycorrect.transcend.reader
   (:require [com.beyondtechnicallycorrect.transcend.grid :as grid]))
 
+(def ^:private displayed-val :displayed-value)
+
 (defn get-displayed-value-at
   [model row col]
-  (:displayed-value (grid/get-grid-value model row col)))
+  (displayed-val (grid/get-grid-value model row col)))
 
 (defn get-displayed-range
   [model [row1 col1] [row2 col2]]
   (map
-    #(map :displayed-value %)
+    #(map displayed-val %)
     (grid/get-grid-range model [row1 col1] [row2 col2])))
